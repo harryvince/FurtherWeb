dbef3c627f21bffcbe8d0bfddcb10147d2e50ccd
harryvince <harryavince@gmail.com>
2021-10-22 11:19:51 +0100

    added profile button

--- phpScripts/adminAuth.php
+++ phpScripts/adminAuth.php
@@ -1,7 +1,10 @@
 <?php
 if(isset($_SESSION['userType'])){
     if ($_SESSION['userType'] == 1){
-        echo"<a class='nav-item nav-link' href='#'>Admin</a>";
+        echo"<a class='nav-item nav-link' style='margin-right:10px' href='#'>Admin</a>";
     }
+}
+if(isset($_SESSION['username'])){
+    echo"<a class='nav-item nav-link' href='#'>Profile</a>";
 }
 ?>
