6c7f5f2bee7cf340d16b5f05ddd94be23ba4a52f
harryvince <harryavince@gmail.com>
2021-10-23 18:15:28 +0100

    Accounts can now be created with the register page

--- RegisterPage.php
+++ RegisterPage.php
@@ -1,4 +1,5 @@
 <html>
+
 <?php
 session_start();
 if(isset($_SESSION['username'])!="")
@@ -10,7 +11,7 @@ if(isset($_SESSION['username'])!="")
 <head>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/logincss.css">
-  <title>Login | Vince Shops</title>
+  <title>Register | Vince Shops</title>
   <link rel="icon" href="images/favicon.ico"/>
   <script src="https://use.typekit.net/rjb4unc.js"></script>
   <script>try{Typekit.load({ async: true });}catch(e){}</script>
@@ -20,12 +21,7 @@ if(isset($_SESSION['username'])!="")
 <body>
     <div class="container">
     <div class="logo"><img src="images/cart.png" style="width:40px;height:40px;margin-top:5px;margin-right:5px;" /></div>
-    <?php if(isset($_SESSION['registered'])){
-      echo "<br>";
-      echo "<br>";
-      echo "<p class='fonts' style='text-align: center; color: green;'>You are now registered, Please login</p>";
-    }
-    require('phpScripts/loginScript.php'); ?>
+    <?php require('phpScripts/registerScript.php'); ?>
     <div class="login-item">
       <form method="post" class="form form-login">
         <div class="form-field">
@@ -39,7 +35,7 @@ if(isset($_SESSION['username'])!="")
         </div>
 
         <div class="form-field">
-          <input type="submit" value="Log in">
+          <input type="submit" value="Register">
         </div>
       </form>
     </div>
---
+++ phpScripts/registerScript.php
@@ -0,0 +1,31 @@
+<?php
+require('connectDB.php');
+require('functions.php');
+error_reporting(E_ERROR | E_PARSE);
+session_start();
+if(isset($_POST['username']) && isset($_POST['password'])) {
+  $username = sanitize(mysqli_real_escape_string($conn, $_POST['username']));
+  $password = sanitize(mysqli_real_escape_string($conn, $_POST['password']));
+
+  $password = hash('sha256', $password);
+
+  $stmt = $conn -> prepare('SELECT username FROM login_table WHERE username = ? LIMIT 1');
+  $stmt -> bind_param('s', $username);
+  $stmt -> execute();
+  $stmt -> bind_result($checkUsername);
+  $stmt -> store_result();
+  if($stmt->num_rows == 1){
+    echo "<br>";
+    echo "<br>";
+    echo "<p class='fonts' style='text-align: center; color: red;'>User account already Exsists!</p>";
+  }
+  else {
+    $insertNewUser = $conn -> prepare('INSERT INTO login_table (username, password, userType) VALUES (?, ?, 0)');
+    $insertNewUser -> bind_param('ss', $username, $password);
+    $insertNewUser -> execute();
+    $_SESSION['registered'] = TRUE;
+    header('Location: loginPage.php');
+  }
+  $stmt->close();
+}
+?>
